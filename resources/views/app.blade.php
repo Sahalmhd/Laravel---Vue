03e88d1305a7c199bbfa0a54ea6be7d446d04e38
Sahal mhd <sahalmhd25@gmail.com>
2026-02-12 14:21:12 +0530

    tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
 
 
     </head>
-    <body>
+    <body class="bg-white dark:bg-gray-900">
         @inertia
         </body>
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
@@ -27,7 +26,6 @@ export default defineConfig({
         },
       },
     }),
-    tailwindcss(),
 
   ],
   resolve: {
