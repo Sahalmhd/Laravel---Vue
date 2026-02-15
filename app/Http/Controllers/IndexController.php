1b701ceeb365549f95da54f6969c87690b4302b4
Sahal mhd <sahalmhd25@gmail.com>
2026-02-15 22:36:03 +0530

    Fix:small bug due to deletation

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,18 +2,4 @@
 
 namespace App\Http\Controllers;
 
-class IndexController extends Controller
-{
-    public function index()
-    {
-        return inertia(
-            'Index',
-            ['message' => 'hello lravel']
-        );
-    }
-
-    public function show()
-    {
-        return inertia('Show');
-    }
-}
+class IndexController extends Controller {}
