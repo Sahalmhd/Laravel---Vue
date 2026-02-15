2983c7bccdeaf5ec1f5fe9881a981c4337521197
Sahal mhd <sahalmhd25@gmail.com>
2026-02-15 14:40:14 +0530

    index page completed

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,19 +1,22 @@
 <template>
-  <div v-for="listing in listings" :key="listing.id">
-    <Link :href="route('listings.show', listing.id)">
-      <ListingAddress :listing="listing" />
-    </Link>
-    <div>
-      <Link :href="route('listings.edit',{listing:listing.id})">Edit</Link>
-    </div>
-    <div>
-      <Link :href="route('listings.destroy',{listing:listing.id})" method="DELETE">Delete</Link>
-    </div>
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <Box v-for="listing in listings" :key="listing.id">
+      <Link :href="route('listings.show', listing.id)">
+        <ListingAddress :listing="listing" />
+      </Link>
+      <div>
+        <Link :href="route('listings.edit',{listing:listing.id})">Edit</Link>
+      </div>
+      <div>
+        <Link :href="route('listings.destroy',{listing:listing.id})" method="DELETE">Delete</Link>
+      </div>
+    </Box>
   </div>
 </template>
 
 <script setup>
 import ListingAddress from '@/Components/ListingAddress.vue'
+import Box from '@/Components/UI/Box.vue'
 import { Link } from '@inertiajs/vue3'
 defineProps({
   listings: Array,
--- resources/js/Pages/Show.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <div>Show</div>
-  </div>
-</template>
-
-<script setup>
-
-</script>
