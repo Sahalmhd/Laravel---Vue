c21b03be1b3e4fa9a60f09fa387cbc61986d17da
Sahal mhd <sahalmhd25@gmail.com>
2026-04-15 19:13:22 +0530

    file

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ListingController;
 use App\Http\Controllers\RealtorListingController;
+use App\Http\Controllers\RealtorListingImageController;
 use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,6 @@ Route::prefix('realtor')->name('realtor.')->middleware('auth')->group(function (
         ->withTrashed()
         ->name('listing.restore');
     Route::resource('listing', RealtorListingController::class)->only(['index', 'create', 'store', 'edit', 'update', 'destroy'])->withTrashed();
+    Route::resource('listing.image', RealtorListingImageController::class)
+        ->only(['create', 'store']);
 });
